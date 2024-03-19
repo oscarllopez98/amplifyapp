2cd45dd0a43eef3e406679365a3e8d25a03ee321
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-03-19 10:19:59 -0700

    Added graphsql, had to update from Storage to using updated implementation that is not outlined in the module

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyapp1ad9c085": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "imagestore": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
